b59cd5fa4934790b83a721643b511e3425f9fc75
RafaelAndradeRC <73724054+RafaelAndradeRC@users.noreply.github.com>
2021-12-10 11:47:53 -0600

    Bug Corregido
    En LongestWishList

--- Santa's Workshop/Scripts/biggestWishlists.js
+++ Santa's Workshop/Scripts/biggestWishlists.js
@@ -42,47 +42,44 @@ function fillDataToys(){
  
   function fillDataToy(){
      var request= new XMLHttpRequest();
+ 	var country = document.getElementById('countryInput');
+    var lk="http://localhost:5000/greedykids?filter="+country.value;
+    request.open('GET',lk);
+    request.onload=function(){
+        var object =JSON.parse(request.response);
+        var jsonTest= object.recordsets;
+        console.log(jsonTest);
  
-     var country = document.getElementById('countyInput');
-     alert('Grouping by country: ' + country.value);
+        console.log(jsonTest.length);
  
-     var lk="http://localhost:5000/bestboys" + "?filter=" + country.value;
-     request.open('GET',lk);
-     request.onload=function(){
-         var object =JSON.parse(request.response);
-         var jsonTest= object.recordsets;
-         console.log(jsonTest);
-  
-         console.log(jsonTest.length);
-  
-      var table = document.getElementById('table');
-  
-      for(var i=0; i<jsonTest[0].length ; i++){
-          var tr = document.createElement('tr');
-          var d = jsonTest[0][i];
-  
-          var name = document.createElement('td');
-          name.innerHTML = d.Name;
-          tr.appendChild(name);
-  
-          var lastName = document.createElement('td');
-          lastName.innerHTML = d.LASTNAME;
-          tr.appendChild(lastName);
-  
-          var country = document.createElement('td');
-          country.innerHTML = d.Country;
-          tr.appendChild(country);
-  
-          var goodness = document.createElement('td');
-          goodness.innerHTML = d.GOODNESS;
-          tr.appendChild(goodness);
-  
-          table.appendChild(tr);
-      }
-      
-  }
-    
-     request.send();
+     var table = document.getElementById('table');
+ 
+     for(var i=0; i<jsonTest[0].length ; i++){
+         var tr = document.createElement('tr');
+         var d = jsonTest[0][i];
+ 
+         var KID_ID = document.createElement('td');
+         KID_ID.innerHTML = d.KID_ID;
+         tr.appendChild(KID_ID);
+ 
+         var NAME = document.createElement('td');
+         NAME.innerHTML = d.NAME;
+         tr.appendChild(NAME);
+ 
+         var LASTNAME = document.createElement('td');
+         LASTNAME.innerHTML = d.LASTNAME;
+         tr.appendChild(LASTNAME);
+ 
+         var nRegalos = document.createElement('td');
+         nRegalos.innerHTML = d.nRegalos;
+         tr.appendChild(nRegalos);
+ 
+         table.appendChild(tr);
+     }
+     
+ }
+   
+    request.send();
   }
  
  function setListeners(){
@@ -90,16 +87,26 @@ function fillDataToys(){
      groupBy.onclick = function () {
          // Call filtered query
          resetTable();
-         fillDataToy();
+	var c = document.getElementById('countryInput');
+	if(c.value==""){
+		fillDataToys();
+	}
+	else{
+		fillDataToy();
+	}
+	
+	
+         
      }
 
  }
  
  function resetTable() {
      var table = document.getElementById('table');
-     var codeBlock = '<table class="table table-striped" style=" margin-top: 10px; background-color: whitesmoke;" id="table">' + '<tr>' + '<th>Kid ID</th>' + '<th>First Name</th>' + '<th>Available</th>' + '<th>Wishlist Length</th>' + '</tr>' + '</table>';
+     var codeBlock = '<table class="table table-striped" style=" margin-top: 10px; background-color: whitesmoke;" id="table">' + '<tr>' + '<th>Kid ID</th>' + '<th>First Name</th>' + '<th>Last Name</th>' + '<th>Wishlist Length</th>' + '</tr>' + '</table>';
      table.innerHTML = codeBlock;
  }
 
- fillDataToys();
+
+
  
